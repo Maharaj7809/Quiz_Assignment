f10eb15b01ea5e93ccd5fd720819cfc706b61ddd
Praveen Maharaj <praveenmaharaj.625@gmail.com>
2024-01-17 22:33:35 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BrowserRouter as Router, Route,Routes } from 'react-router-dom';
+
+import Home from './components/Home';
+import QuizInstructions from './components/quiz/QuizInstructions';
+import Play from './components/quiz/Play';
+import QuizSummary from './components/quiz/QuizSummary';
+
+function App() {
+  return (
+    <Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/play/instructions" element={<QuizInstructions />} />
+      <Route path="/play/quiz" element={<Play />} />
+      <Route path="/play/quizSummary" element={<QuizSummary />} />
+    </Routes>
+  </Router>
+  );
+}
+
+export default App;
